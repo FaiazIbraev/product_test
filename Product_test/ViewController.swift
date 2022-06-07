2d39be1455a54e3d0a09dda3001fd1641ccfdd4f
Student <student@Sultans-iMac.local>
2022-06-07 19:51:14 +0600

    first commit

---
+++ Product_test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Product_test
+//
+//  Created by Student on 7/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+}
+
